edee74488569b166e3f6e59f325116c057310917
Ridge <rto@MBAir.local>
2017-05-23 17:52:48 -0700

    add a new screen to navigate to

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import {
   AppRegistry,
+  View,
+  Button,
   Text,
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+
+class NextScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Next',
+  };
+  render() {
+    return (
+      <View>
+        <Text>This is the NextScreen</Text>
+      </View>
+    );
+  }
+}
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Home',
   };
   render() {
-    return <Text>Hello, Navigation!</Text>;
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>Hello, Navigation!</Text>
+        <Button
+          onPress={() => navigate('Next')}
+          title="Next Screen"
+        />
+      </View>
+    );
   }
 }
 
 const ReactNav = StackNavigator({
   Home: { screen: HomeScreen },
+  Next: { screen: NextScreen }
 });
 
 AppRegistry.registerComponent('ReactNav', () => ReactNav);
