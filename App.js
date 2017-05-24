ccb0973a38a92110deba38e9c811e631926b01e1
Ridge <rto@MBAir.local>
2017-05-23 17:45:37 -0700

    add a homescreen

--- App.js
+++ App.js
@@ -1,47 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   AppRegistry,
-  StyleSheet,
   Text,
-  View
 } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class ReactNav extends Component {
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home',
+  };
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+    return <Text>Hello, Navigation!</Text>;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const ReactNav = StackNavigator({
+  Home: { screen: HomeScreen },
 });
 
 AppRegistry.registerComponent('ReactNav', () => ReactNav);
